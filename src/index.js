f0922bc8d809b06fd58e730be3fdb4a2de27233a
sraut <sraut@overstock.com>
2016-11-19 19:18:47 -0700

    reducers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
-// Create a new component. This component should produce some HTML
-const App = function() {
-    return <div>Hi!</div>;
+/**
+ * Created by sraut on 11/13/16.
+ */
+
+import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+import App from './components/app';
+
+//import container from '/Users/sraut/Projects/ReduxSimpleStarter/index';
+//Create a new component
+    //html into the javascript function is JSX code and which will be translated by webpack+babel
+
+class App extends Component {
+    render() {
+        return(
+            <div>
+                <App />
+            </div>
+        );
+    }
 }
-// Take this component's generated HTML and put it
-// on the page (in the DOM)
+
+
+// Take this component to show onto the browser(in the DOM)
+ReactDOM.render(<App />, document.querySelector('.containers'));
 
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+    return [
+        { title: 'Java Script book', pages: '121' },
+        { title: 'C++', pages: '101' },
+        { title: 'Core Java', pages: '12' },
+        { title: 'React-Redux', pages: '23' }
+    ]
+
+}
